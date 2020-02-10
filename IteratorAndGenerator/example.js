99115f689e962df31363d37250920d04dd03e4d8
Tung_DK <tung_dk@septeni-technology.jp>
2020-02-10 17:56:58 +0700

    iterator in TS

---
+++ IteratorAndGenerator/example.js
@@ -0,0 +1,5 @@
+var infor = [1, "abcd", false];
+for (var _i = 0, infor_1 = infor; _i < infor_1.length; _i++) {
+    var iter = infor_1[_i];
+    console.log(iter); // 1, "tung", false
+}
---
+++ IteratorAndGenerator/example.ts
@@ -0,0 +1,5 @@
+let infor = [1, "abcd", false];
+
+for (let iter of infor) {
+    console.log(iter); // 1, "tung", false
+}
